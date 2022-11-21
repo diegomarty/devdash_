2627b97559d2ca9860dab9ee7af4d4d08e217c93
DiegoMarty <contacto@diegomarty.com>
2022-11-21 16:40:10 +0100

    ⚡️ Added code from repo when curse is off

--- src/sections/dashboard/Dashboard.tsx
+++ src/sections/dashboard/Dashboard.tsx
@@ -1,14 +1,18 @@
 import { githubApiResponses } from "../../github_api_responses";
+import { ReactComponent as Brand } from "./brand.svg";
 import { ReactComponent as Check } from "./check.svg";
 import styles from "./Dashboard.module.scss";
 import { ReactComponent as Error } from "./error.svg";
+import { ReactComponent as PullRequests } from "./git-pull-request.svg";
+import { ReactComponent as IssueOpened } from "./issue-opened.svg";
 import { ReactComponent as Lock } from "./lock.svg";
+import { ReactComponent as Forks } from "./repo-forked.svg";
+import { ReactComponent as Start } from "./star.svg";
 import { ReactComponent as Unlock } from "./unlock.svg";
+import { ReactComponent as Watchers } from "./watchers.svg";
 
 export function Dashboard() {
-	const title = "DevDash_";
-
-	const isoToReadable = (lastUpdate: string): string => {
+	const isoToReadableDate = (lastUpdate: string): string => {
 		const lastUpdatedDate = new Date(lastUpdate);
 		const currentDate = new Date();
 		const diffDays = lastUpdatedDate.getDate() - currentDate.getDate();
@@ -26,8 +30,11 @@ export function Dashboard() {
 
 	return (
 		<>
-			<header className={styles.container}>
-				<h1>{title}</h1>
+			<header className={styles.header}>
+				<section className={styles.header__container}>
+					<Brand />
+					<h1 className={styles.app__brand}>DevDash_</h1>
+				</section>
 			</header>
 			<section className={styles.container}>
 				{githubApiResponses.map((widget) => (
@@ -44,12 +51,43 @@ export function Dashboard() {
 							</a>
 							{widget.repositoryData.private ? <Lock /> : <Unlock />}
 						</header>
-						<p>Last update {isoToReadable(widget.repositoryData.updated_at)}</p>
-						{widget.CiStatus.workflow_runs.length > 0 && (
-							<div>
-								{widget.CiStatus.workflow_runs[0].status === "completed" ? <Check /> : <Error />}
+						<div className={styles.widget__body}>
+							<div className={styles.widget__status}>
+								<p>Last update {isoToReadableDate(widget.repositoryData.updated_at)}</p>
+								{widget.CiStatus.workflow_runs.length > 0 && (
+									<div>
+										{widget.CiStatus.workflow_runs[0].status === "completed" ? (
+											<Check />
+										) : (
+											<Error />
+										)}
+									</div>
+								)}
 							</div>
-						)}
+							<p className={styles.widget__description}>{widget.repositoryData.description}</p>
+						</div>
+						<footer className={styles.widget__footer}>
+							<div className={styles.widget__stat}>
+								<Start />
+								<span>{widget.repositoryData.stargazers_count}</span>
+							</div>
+							<div className={styles.widget__stat}>
+								<Watchers />
+								<span>{widget.repositoryData.watchers_count}</span>
+							</div>
+							<div className={styles.widget__stat}>
+								<Forks />
+								<span>{widget.repositoryData.forks_count}</span>
+							</div>
+							<div className={styles.widget__stat}>
+								<IssueOpened />
+								<span>{widget.repositoryData.open_issues_count}</span>
+							</div>
+							<div className={styles.widget__stat}>
+								<PullRequests />
+								<span>{widget.pullRequest.length}</span>
+							</div>
+						</footer>
 					</article>
 				))}
 			</section>
